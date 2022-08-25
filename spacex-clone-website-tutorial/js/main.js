339894d734974fd99da493cdc3495664f6237830
brittonstoffregen <britton@wadeh.com>
2022-08-25 15:30:25 -0500

    TUTORIAL creating first inner page (falcon9.html) + adding javascript stats section to falcon9.html

--- spacex-clone-website-tutorial/js/main.js
+++ spacex-clone-website-tutorial/js/main.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById('menu-btn')
 const overlay = document.getElementById('overlay')
 const menu = document.getElementById('mobile-menu')
+const counters = document.querySelectorAll('.counter')
+
 
 btn.addEventListener('click', navToggle)
 
@@ -10,4 +12,50 @@ function navToggle() {
   overlay.classList.toggle('overlay-show')
   document.body.classList.toggle('stop-scrolling')
   menu.classList.toggle('show-menu')
+}
+
+/* Inner Pages Stats Section Scroll */
+
+document.addEventListener('scroll', scrollPage)
+
+let scrollStarted = false
+
+function scrollPage() {
+  const scrollPos = window.scrollY
+
+  if(scrollPos > 100 && !scrollStarted) {
+    countUp()
+    scrollStarted = true
+  } else if(scrollPos < 100 && scrollStarted) {
+    reset()
+    scrollStarted = false
+  }
+}
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target')
+      
+      const c = +counter.innerText
+
+      const increment = target / 100
+
+      if(c < target) {
+        counter.innerText = `${Math.ceil(c + increment)}`
+
+        setTimeout(updateCounter, 75)
+      } else {
+        counter.innerText = target
+      }
+    };
+
+    updateCounter()
+  });
+}
+
+function reset() {
+  counters.forEach((counter)  => counter.innerHTML = '0') 
 }
