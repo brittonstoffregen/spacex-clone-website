70080aff511439bf3377f9f5aa0c5b89d511a734
brittonstoffregen <britton@wadeh.com>
2022-08-25 13:04:11 -0500

    SCRATCH adding javascript and mobile menu to scratch site

---
+++ spacex-clone-website-scratch/js/main.js
@@ -0,0 +1,11 @@
+const hamburgerBtn = document.getElementById('menu-btn')
+const overlay = document.getElementById('overlay-el')
+const mobileMenu = document.getElementById('mobile-menu')
+
+hamburgerBtn.addEventListener('click', toggleHamburgerMenu)
+
+function toggleHamburgerMenu() {
+  hamburgerBtn.classList.toggle('open')
+  overlay.classList.toggle('overlay-show')
+  mobileMenu.classList.toggle('open-menu')
+}
